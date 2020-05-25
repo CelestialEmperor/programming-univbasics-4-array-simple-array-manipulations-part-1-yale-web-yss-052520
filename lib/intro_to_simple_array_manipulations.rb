42e9ddac01bc18730220566aec72520240f3d7bd
Jack Ma <mapeiyao@gmail.com>
2020-05-25 10:49:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,7 @@ def using_pop(array)
   array.pop()
 end
 
-def pop_with_args(array, string)
+def pop_with_args(array, 2)
   array.pop[2]
 end
 
@@ -18,6 +18,6 @@ def using_shift(array)
   array.shift()
 end
 
-def shift_with_args
-  array.shift[2]
+def shift_with_args(array, 2)
+  array.shift(2)
 end
